6a288900d3563de7b4958a52b626ab5cf4c61fba
ElHabib <cherq.habib@gmail.com>
2018-08-25 21:00:36 +0100

    heroku

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ app.use(express.static('./dist/test-project'));
 
 app.get('/*', (req,res)=>{
 
-  res.sendFile(path.join(__dirname,'/dist/test-project/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/test-project/index.html'));
 
 });
 
