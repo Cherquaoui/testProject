78031cf3383bc409f965b66cc44d2a76ec052896
ElHabib <cherq.habib@gmail.com>
2018-08-25 19:59:56 +0100

    heroku

---
+++ express.js
@@ -0,0 +1,19 @@
+
+
+const express= require('express'),
+  path = require('path');
+
+const app =express();
+
+
+app.use(express.static('./dist/test-project'));
+
+app.get('/*', (req,res)=>{
+
+  res.sendFile(path.join(__dirname,'/dist/test-project/index.html'));
+
+});
+
+app.listen(process.env.PORT || 8080, ()=>{
+  console.log('Server started');
+})
